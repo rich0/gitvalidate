4896955601dcb2ad89da895144f6407519539b39
Richard Freeman <rich0@gentoo.org>
2012-10-07 16:30:02 -0400

    Initial tree walk implementation.  Not fully tested.

--- gitdump/gitdump.py
+++ gitdump/gitdump.py
@@ -6,6 +6,7 @@
 import sys;
 import os;
 import subprocess;
+import csv;
 
 repo = '/home/rich/sstore3/gentoo-gitmig/git/gentoo-x86/'
 head = 'c353557f65c845fd25ddda3b0ea9065be77c4a20'
@@ -13,12 +14,12 @@ head = 'c353557f65c845fd25ddda3b0ea9065be77c4a20'
 def parsecommit(repo,commit):
 	os.chdir(repo);
 	output = subprocess.check_output(["git","cat-file","commit",commit]);
-	print output;
+#	print output;
 
 	message = "";
 
 	for line in output.split("\n"):
-	   print line;
+#	   print line;
 	   if len(message) > 0:
 	      message = message + "\n" + line;
 	   elif line.startswith("tree"):
@@ -34,16 +35,30 @@ def parsecommit(repo,commit):
 	      message = line;
 
 
-	print tree, parent;
-	print author;
-	print time, tz;
-	print message;
+#	print tree, parent;
+#	print author;
+#	print time, tz;
+#	print message;
 
 	gitobject = "tree","",tree,time,tz,author,message
 	return parent,gitobject
 
-parent, info = parsecommit(repo, head);
-print "parent: ",parent;
-objtype, name, hashdata, time, tz, author, message = info;
-print info;
+def parsetree(head):
+	parent = head;
+	#print parent;
+	gitobjs = [];
+	while parent != "":
+		parent, info = parsecommit(repo, parent);
+	#	print "parent: ",parent;
+		gitobjs.append(info);
+
+	return gitobjs;
+
+gitobs = parsetree(head);
+
+with open('/sstorage3/tmp/outfile.csv') as outcsv:
+	outfile = csv.writer(outcsv);
+	outfile.writerows(gitobjs);
+
+#print gitobjs;
 
