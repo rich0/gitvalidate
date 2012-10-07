4c86a259ed5fee07d096652741fb3182dcc8e4fd
Richard Freeman <rich0@gentoo.org>
2012-10-07 17:41:14 -0400

    Using git log to greatly speed commit parsing

--- gitdump/gitdump.py
+++ gitdump/gitdump.py
@@ -11,54 +11,81 @@ import csv;
 repo = '/home/rich/sstore3/gentoo-gitmig/git/gentoo-x86/'
 head = 'c353557f65c845fd25ddda3b0ea9065be77c4a20'
 
-def parsecommit(repo,commit):
+def oldparsecommit(repo,commit):
 	os.chdir(repo);
 	output = subprocess.check_output(["git","cat-file","commit",commit]);
-#	print output;
 
 	message = "";
 
 	for line in output.split("\n"):
-#	   print line;
-	   if len(message) > 0:
-	      message = message + "\n" + line;
-	   elif line.startswith("tree"):
-	      dummy, tree = line.split();
-	   elif line.startswith("parent"):
-	      dummy, parent = line.split();
-	   elif line.startswith("author"):
-	      dummy, right = line.split(" ",1);
-	      author, time, tz = right.rsplit(" ",2);
-	   elif line.startswith("committer"):
-	      dummy = "";
-	   elif (len(line) > 0) and (len(message) == 0):
-	      message = line;
-
-
-#	print tree, parent;
-#	print author;
-#	print time, tz;
-#	print message;
+		if len(message) > 0:
+			message = message + "\n" + line;
+		elif line.startswith("tree"):
+			dummy, tree = line.split();
+		elif line.startswith("parent"):
+			dummy, parent = line.split();
+		elif line.startswith("author"):
+			dummy, right = line.split(" ",1);
+			author, time, tz = right.rsplit(" ",2);
+		elif line.startswith("committer"):
+			dummy = "";
+		elif (len(line) > 0) and (len(message) == 0):
+			message = line;
 
 	gitobject = "tree","",tree,time,tz,author,message
 	return parent,gitobject
 
-def parsetree(head):
+def oldparsetree(repo,head):
 	parent = head;
-	#print parent;
 	gitobjs = [];
 	while parent != "":
 		parent, info = parsecommit(repo, parent);
-	#	print "parent: ",parent;
 		gitobjs.append(info);
 
 	return gitobjs;
 
-gitobs = parsetree(head);
 
-with open('/sstorage3/tmp/outfile.csv') as outcsv:
-	outfile = csv.writer(outcsv);
-	outfile.writerows(gitobjs);
 
-#print gitobjs;
+def parsetree(repo):
+	os.chdir(repo);
+	gitobjs=[];
+
+	output = subprocess.check_output(["git","log","--pretty=raw"]);
+
+	message = "";
+	skip1=True
+
+	for line in output.split("\n"):
+		if line.startswith("commit ") and not skip1:
+			gitobject = "tree","",tree,time,tz,author,message;
+			gitobjs.append(gitobject);
+			message="";
+		elif line.startswith("commit "):
+			skip1=False;
+		elif len(message) > 0:
+			message = message + "\n" + line;
+		elif line.startswith("tree"):
+			dummy, tree = line.split();
+		elif line.startswith("parent"):
+			dummy, parent = line.split();
+		elif line.startswith("author"):
+			dummy, right = line.split(" ",1);
+			author, time, tz = right.rsplit(" ",2);
+		elif line.startswith("committer"):
+			dummy = "";
+		elif (len(line) > 0) and (len(message) == 0):
+			message = line;
+
+
+	gitobject = "tree","",tree,time,tz,author,message
+	gitobjs.append(gitobject);
+
+	return gitobjs
+
+
+gitobjs = parsetree(repo);
+
+#with open('/sstorage3/tmp/outfile.csv', 'wb') as outcsv:
+#	outfile = csv.writer(outcsv);
+#	outfile.writerows(gitobjs);
 
