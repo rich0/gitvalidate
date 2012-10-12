fab636887a504dab9c3327bcf1afc12480dfc224
Richard Freeman <rich0@gentoo.org>
2012-10-12 14:51:21 -0400

    Update pygit installer to use /usr prefix.

--- gitdump/starcluster/install_pygit.sh
+++ gitdump/starcluster/install_pygit.sh
@@ -11,7 +11,7 @@ cd libgit2
 
 # instructions from http://libgit2.github.com/#install
 mkdir build && cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ..
 cmake --build .
 sudo cmake --build . --target install
 
@@ -20,11 +20,11 @@ cd ../../
 # instructions from https://github.com/libgit2/pygit2/blob/master/README.rst
 git clone git://github.com/libgit2/pygit2.git
 cd pygit2
-sudo python setup.py install
+sudo python setup.py install --prefix=/usr
 
 # avoid "cannot open shared object file" ImportErrors; make sure the libraries are accessible in LD_LIBRARY_PATH
 # if you want a more permanent solution, try echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH` >> ~/.bashrc
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 # clean up after ourselves
 cd ../../
