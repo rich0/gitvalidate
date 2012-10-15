85683ac0722d2cdd18d018c9a128eadfa9a73874
Richard Freeman <rich0@gentoo.org>
2012-10-15 13:48:16 -0400

    Modify parsetrees to dump to stdout.

--- gitdump/parsetrees.py
+++ gitdump/parsetrees.py
@@ -22,8 +22,8 @@ current = repository.head;
 currentdepth=0;
 depth=int(depth);
 
-outcsv=open('/sstorage3/tmp/outfile.csv', 'wb');
-outfile=csv.writer(outcsv);
+#outcsv=open('/sstorage3/tmp/outfile.csv', 'wb');
+outfile=csv.writer(sys.stdout);
 
 while currentdepth<depth:
 	message=base64.b64encode(current.message.encode('utf-8'));
@@ -38,31 +38,4 @@ while currentdepth<depth:
 	else:
 		break;
 	currentdepth=currentdepth+1;
-
-
-
-if False:
-	with open('/sstorage3/tmp/outfile.csv', 'wb') as outcsv:
-		outfile = csv.writer(outcsv);
-		for row in gitobjs:
-			r0,r1,r2,r3,r4,r5 = row;
-			r2=r2.encode('utf-8');
-			r4=r4.encode('utf-8');
-			r5=r5.encode('utf-8');
-			newrow=r0,r1,r2,r3,r4,r5;
-			outfile.writerow(newrow);
-
-if False:
-	newobjs=[];
-	with open('/sstorage3/tmp/outfile.csv','rb') as incsv:
-		infile=csv.reader(incsv);
-		for row in infile:
-			r0,r1,r2,r3,r4,r5 = row;
-			r2=r2.decode('utf-8');
-			r3=int(r3);
-			r4=r4.decode('utf-8');
-			r5=r5.decode('utf-8');
-			newrow=r0,r1,r2,r3,r4,r5;	
-			newobjs.append(newrow);
-	print gitobjs==newobjs;
 
