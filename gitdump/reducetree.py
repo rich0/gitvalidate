3dab11c99de922428281b1bb6879d81e63ed2957
Richard Freeman <rich0@gentoo.org>
2012-10-15 22:11:36 -0400

    Fix line-dropping bug in reducetree.

--- gitdump/reducetree.py
+++ gitdump/reducetree.py
@@ -26,39 +26,44 @@ def csv2stringrows(data):
     return si.getvalue();
 
 # reducer input: key=filename, values=collection of strings of all.  Output key=filename, value=all.
+
+def reducevalues(values):
+	# first convert values to list
+	gitobjs=[];
+	for row in csv.reader(values):
+		gitobjs.append(row);
+
+	gitobjs=sorted(gitobjs, key=itemgetter(0,3));
+	newgitobjs=[];
+
+	firstitem=True;
+	while len(gitobjs) > 0:
+		if not firstitem:
+			nextitem=gitobjs.pop();
+			if nextitem[2] != lastitem[2]:
+				newgitobjs.append(lastitem);
+			lastitem=nextitem;
+		else:
+			firstitem=False;
+			lastitem=gitobjs.pop();
+	newgitobjs.append(lastitem);
+
+	print csv2stringrows(newgitobjs);
+
+
+
 values=[];
 lastkey="";
 for line in sys.stdin:
 	line=line.strip();
 	key,value = line.split('\t',1);
 	if key!=lastkey and len(values)>0:
-
-		# first convert values to list
-		gitobjs=[];
-		for row in csv.reader(values):
-			gitobjs.append(row);
-
-		gitobjs=sorted(gitobjs, key=itemgetter(0,3));
-		newgitobjs=[];
-
-		firstitem=True;
-		while len(gitobjs) > 0:
-			if not firstitem:
-				nextitem=gitobjs.pop();
-				if nextitem[2] != lastitem[2]:
-					newgitobjs.append(lastitem);
-				lastitem=nextitem;
-			else:
-				firstitem=False;
-				lastitem=gitobjs.pop();
-		newgitobjs.append(lastitem);
-
-		print csv2stringrows(newgitobjs);
+		reducevalues(values)
 
 		values=[];
 		lastkey=key;
 	else:
 		lastkey=key;
 	values.append(value);
-
+reducevalues(values);
 
