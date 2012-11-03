42c18760f1f13ad2507d3053b4a32ef025d7a5dd
Richard Freeman <rich0@gentoo.org>
2012-11-03 14:28:05 -0400

    Clean indents, remove unused modules.

--- cvsdump/cvscalchash.py
+++ cvsdump/cvscalchash.py
@@ -14,21 +14,7 @@ import sys;
 import os;
 import subprocess;
 import csv;
-import multiprocessing;
-import itertools;
-import datetime;
-import time;
-import base64;
-import iso8601;
 import hashlib;
-from operator import itemgetter, attrgetter;
-
-def csv2string(data):
-    si = StringIO.StringIO();
-    cw = csv.writer(si);
-    cw.writerow(data);
-    return si.getvalue().strip('\r\n');
-
 
 repo = '/home/rich/sstore3/gentoo-gitmig/cvs/checkout/gentoo-x86/'
 module = 'gentoo-x86/'
@@ -37,17 +23,15 @@ outfile=csv.writer(sys.stdout);
 os.chdir(repo);
 
 for line in sys.stdin:
-	line=line.strip();
-	if len(line) > 0:
-		for row in csv.reader([line]):
-#			print row;
-			filename,filetime,author,message,revision,state = row
-			if state != "dead":
-				output=subprocess.check_output(["cvs","co","-p","-r",revision,module+filename]).split("\n")
-				output='\n'.join(output)
-				final="blob "+str(len(output))+"\x00"+output;
-				filehash=hashlib.sha1(final).hexdigest();
-				newrow=filename,"blob",filehash,filetime,author,message
-				outfile.writerow(newrow)
-
+    line=line.strip();
+    if len(line) > 0:
+        for row in csv.reader([line]):
+            filename,filetime,author,message,revision,state = row
+            if state != "dead":
+                output=subprocess.check_output(["cvs","co","-p","-r",revision,module+filename]).split("\n")
+                output='\n'.join(output)
+                final="blob "+str(len(output))+"\x00"+output;
+                filehash=hashlib.sha1(final).hexdigest();
+                newrow=filename,"blob",filehash,filetime,author,message
+                outfile.writerow(newrow)
 
