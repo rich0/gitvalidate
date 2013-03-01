f4a99ca928f5903422cd27c16be7000fc13f8d53
Richard Freeman <rich0@gentoo.org>
2013-02-28 19:31:20 -0500

    Add dos2unix in stream.

--- cvsdump/cvsprocesstree.sh
+++ cvsdump/cvsprocesstree.sh
@@ -4,4 +4,4 @@
 # $2 - path to cvs checkout of gentoo-x86
 
 cd "$1"
-find | grep ",v" | parallel cvslogparse.sh | parallel --pipe cvscalchash.py "$2" gentoo-x86/  | sort -t , -k 1,1 -k 2,2rn | csvreduce.py | sed '/^$/d'
+find | grep ",v" | parallel cvslogparse.sh | parallel --pipe cvscalchash.py "$2" gentoo-x86/  | sort -t , -k 1,1 -k 2,2rn | csvreduce.py | dos2unix | sed '/^$/d'
